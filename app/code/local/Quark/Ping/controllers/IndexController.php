1d5fcf15620615b553e582e3c35698d068127b06
Abiral Shakya <shakyaabiral@gmail.com>
2018-08-31 11:16:37 +1000

    Prevent `/ping` endpoint from creating new session

--- app/code/local/Quark/Ping/controllers/IndexController.php
+++ app/code/local/Quark/Ping/controllers/IndexController.php
@@ -2,6 +2,16 @@
 
 class Quark_Ping_IndexController extends Mage_Core_Controller_Front_Action
 {
+    public function preDispatch()
+    {
+        $this->getLayout()->setArea($this->_currentArea);
+        $this->setFlag('', self::FLAG_NO_START_SESSION, 1); // Do not start standard session
+        $this->setFlag('', self::FLAG_NO_PRE_DISPATCH, 1);
+        $this->setFlag('', self::FLAG_NO_POST_DISPATCH, 1);
+        parent::preDispatch();
+        return $this;
+    }
+    
     /**
      * Index action
      */
